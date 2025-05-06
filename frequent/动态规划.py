4aafba7710feeca4c09bf70c6278ce8795668afd
yangyang <yangyang111203@163.com>
2025-05-06 21:50:21 +0800

    20250506

--- frequent/动态规划.py
+++ frequent/动态规划.py
@@ -30,3 +30,15 @@ class Solution:
                     res = max(res, dp[i][j])
         return res
 
+    # 300. 最长递增子序列
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+        res = 1
+        for i in range(1, n):
+            for j in range(0, i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    res = max(res, dp[i])
+        return  res
+
---
+++ frequent/图.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+
+    # 207. 课程表
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pass
+
+
+
+    # 210. 课程表 II
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        pass
+
