b386e0d94e71363bd348047047ae1ac2d6b0e0bb
yangyang154 <yangyang154@58.com>
2025-05-06 16:20:39 +0800

    fix

--- frequent/图.py
+++ frequent/图.py
@@ -20,3 +20,23 @@ class Solution:
                 if inDegree[q] == 0:
                     p.append(q)
         return res == numCourses
+
+    # 210. 课程表 II
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        dic = collections.defaultdict(list)
+        inDegree = {i:0 for i in range(numCourses)}
+        for pre in prerequisites:
+            dic[pre[1]].append(pre[0])
+            inDegree[pre[0]] += 1
+        p = collections.deque([key for key, value in inDegree.items() if value == 0])
+        res = []
+        while p:
+            node = p.popleft()
+            res.append(node)
+            for q in dic[node]:
+                inDegree[q] -= 1
+                if inDegree[q] == 0:
+                    p.append(q)
+        if len(res) == numCourses:
+            return res
+        return []
