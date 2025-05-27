6f056efd4f72ccfc206c5284410492acbdf348df
yangyang154 <yangyang154@58.com>
2025-05-27 17:10:11 +0800

    20250527

--- frequent/字符串.py
+++ frequent/字符串.py
@@ -1,4 +1,5 @@
 import collections
+from typing import List
 
 
 class Solution:
@@ -16,3 +17,19 @@ class Solution:
                 if num == len(dic):
                     res = max(res, j - i + 1)
         return res
+
+
+    # 209
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left = 0
+        n = len(nums)
+        res = n + 1
+        sum_num = 0
+        for right in range(n):
+            sum_num += nums[right]
+            while sum_num >= target:
+                res = min(res, right - left + 1)
+                sum_num -= nums[left]
+                left += 1
+        return res if res < n + 1 else 0
+
